b5a3ea0415362703a4710fe9211a65e259f613ae
mdctenga <mdctenga@gmail.com>
2015-06-24 19:26:41 -1000

    Created a blank array, looped through userEmails, and put the user emails into the array.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var emailList = require('./email_log.json');
 
+// verify that I'm getting the data from the .json file
 console.log(emailList.emails[0].email);
 
-for (var i = 0; i <= emailList.emails.length; i += 1) {
-  var disDupe =
-};
+var userEmails = [];
+// loop through the array
+for (var i = 0; i <= emailList.emails.length - 1; i += 1) {
+  userEmails.push(emailList.emails[i].email);
+}
+
+
+// userEmails.indexOf()
